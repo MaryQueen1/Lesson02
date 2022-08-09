8c9f7c39fc02977c8da839fb48d3a5b9cc632dea
Mary <maryqueenk1@gmail.com>
2022-08-09 21:40:03 +0300

    Добавила 2й пример урока 2 с решением

---
+++ L2Ex03/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 23, 54, 4, 654, 76, 2, 4};// определим новый массив
+
+int n = array.Length; // (кол-во эл. массива)
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
