be878829ddb024a30a628b8cdd445f3fa58718b9
Mary <maryqueenk1@gmail.com>
2022-08-14 15:49:45 +0300

    Добавление задачи 13 (дз)

---
+++ HW2Ex02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Задача 13");
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100)
+{
+    string stringNumber = Convert.ToString(number);
+    Console.WriteLine("Третья цифра заданного числа -> " + stringNumber[2]);
+}
+
+else
+{
+    Console.Write("Третьей цифры нет");
+}
