16a9fb1155e3f39467d63713ef18acd00930e034
Mary <maryqueenk1@gmail.com>
2022-08-14 15:29:27 +0300

    Добавление задачи 10 (дз)

--- HW2Ex01/Program.cs
+++ HW2Ex01/Program.cs
@@ -3,3 +3,19 @@
 // 782 -> 8
 // 918 -> 1
 
+Console.Clear();
+Console.WriteLine("Задача 10 ");
+
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+if (threeDigitNumber >= 100 & threeDigitNumber <= 999)
+{
+    string stringNumber = Convert.ToString(threeDigitNumber);
+    Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+}
+
+else
+{
+    Console.Write("Введите трехначное число!");
+}
