c4e5f8884b489a5d1e982b5c3bf37d7c85f15d8e
Mary <maryqueenk1@gmail.com>
2022-08-14 16:39:25 +0300

    Добавление задачи 15 (дз)

---
+++ HW2Ex03/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.WriteLine("Задача 15");
+
+Console.Write("Enter your number 1-7");
+int a;
+do Console.Write(" (a) = ");
+while (!int.TryParse(Console.ReadLine(), out a));
+
+if(a == 6 || a == 7)
+{
+    Console.WriteLine("Yes");
+}
+
+else if(a < 1 || a > 7)
+{
+    Console.WriteLine("Enter number 1-7!");
+}
+
+else
+{
+    Console.Write("No");
+}
+
